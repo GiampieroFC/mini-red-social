f8339d55ab0f3099bb6ff36bec492fee515f22ea
GiampieroFC <giampieroferminicastillo@gmail.com>
2024-10-18 21:07:39 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotificationsModule } from './notifications/notifications.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { LikesModule } from './likes/likes.module';
+import { CommentsModule } from './comments/comments.module';
+
+@Module({
+  imports: [NotificationsModule, UsersModule, PostsModule, LikesModule, CommentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
